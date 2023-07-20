91d0e0c2cb65422ac664c38f041d5505b5261c51
Mohammad Z Rahmani <rahmani.zalmai@gmail.com>
2023-07-20 12:23:25 +0200

    Install Ziggy for routing

--- resources/js/Layouts/MainLayout.vue
+++ resources/js/Layouts/MainLayout.vue
@@ -1,7 +1,9 @@
  <template>
+    <header class="">
  <Link href="/">Index</Link>&nbsp;
  <Link href="/category">Categories</Link>&nbsp;
  <Link href="/about">About</Link>
+    </header>
 
  <slot>Default</slot>
  </template>
